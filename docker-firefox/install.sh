4e6acbc9629e232bcbba5fe2dbd37d7f0445cd3f
Christian Zufferey <christian@zufferey.com>
2018-12-19 15:03:02 +0100

    Tout début de mon projet ressenti, le docker-firefox fonctionne, ne reste plus que le reste ;-)

---
+++ docker-firefox/install.sh
@@ -0,0 +1,25 @@
+#Petit script pour lancer le binz
+#zf181219.1452
+
+docker build -t "docker-firefox-zf" .
+docker run -d -i -v `pwd`:/root/work --name="docker-firefox" docker-firefox-zf
+
+
+echo -e "
+
+
+Après il faut faire:
+
+docker exec -it docker-firefox /bin/bash
+
+puis à l'intérieur du container:
+
+./test1.sh
+
+ou directement ceci:
+
+docker exec -it docker-firefox /bin/bash /root/work/test1.sh
+
+
+"
+
