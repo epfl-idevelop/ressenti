b6b11cf3d32e99c6bcc3073b27a21e82ad41c838
Christian Zufferey <christian@zufferey.com>
2018-12-20 14:15:07 +0100

    Les mesures de temps ont l'air de bien marcher, mais elles ne sont pas encore poussées dans Prometheus :-(

--- screenshot.sh
+++ screenshot.sh
@@ -1,5 +1,5 @@
 #Petit script pour faire une copie d'écran d'une page web avec firefox en mode headless
-#zf181220.1124
+#zf181220.1359
 
 #test si l'argument est vide
 if [ -z "$1" ]
@@ -8,18 +8,24 @@ if [ -z "$1" ]
     exit
 fi
 
-#time stamp du test
-#ztime=`date +%Y%m%d.%H%M%S-%N`
-
-#mkdir -p ./poubelle/$ztime
-#cd ./poubelle/$ztime
-
 #efface le cache de Firefox
 rm -rf .cache/
 
 zpath="/root/work/images"
 mv $zpath/$2.t1.png $zpath/$2.t0.png 2>/dev/null
 
+zt1=`date +%s.%N`
+firefox -headless -screenshot about-blank.png about:blank 2>/dev/null
+zt2=`date +%s.%N`
+zduree=`jq -n $zt2-$zt1`
+#echo -e "1er shot:" $zduree
+
+zt1=`date +%s.%N`
+firefox -headless -screenshot about-blank.png about:blank 2>/dev/null
+zt2=`date +%s.%N`
+zduree=`jq -n $zt2-$zt1`
+#echo -e "2e shot:" $zduree
+zoverlay=$zduree
 
 zt1=`date +%s.%N`
 firefox -headless -screenshot $zpath/$2.t1.png $1 2>/dev/null
@@ -27,9 +33,7 @@ zt2=`date +%s.%N`
 
 chmod 777 $zpath/$2.t1.png
 
-zduree=`jq -n $zt2-$zt1`
-
-#rm -Rf test*
+zduree=`jq -n $zt2-$zt1-$zoverlay`
 
 echo -e $zduree
 # > duree.txt
--- test1.sh
+++ test1.sh
@@ -1,20 +1,27 @@
 #Petit script pour tester juste la durée d'une page 
-#zf181220.1128
+#zf181220.1410
 
-#time stamp du test
-ztime=`date +%Y%m%d.%H%M%S-%N`
+echo -e "z.zufferey.com"
+    docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh http://z.zufferey.com z.zufferey.com
 
-zt1=`date +%s.%N`
 echo -e "www.epfl.ch"
     docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://www.epfl.ch www.epfl.ch
-zt2=`date +%s.%N`
-zduree=`jq -n $zt2-$zt1`
-echo -e $ztime" "$zduree
 
-zt1=`date +%s.%N`
+echo -e "www.epfl.ch/wp-admin"
+    docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://www.epfl.ch/wp-admin www.epfl.ch-wp-admin
+
+echo -e "actu.epfl.ch"
+    docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://actu.epfl.ch actu.epfl.ch
+
+echo -e "memento.epfl.ch"
+    docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://memento.epfl.ch memento.epfl.ch
+
+echo -e "people.epfl.ch"
+    docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://people.epfl.ch/christian.zufferey people.epfl.ch
+
 echo -e "gel.epfl.ch"
     docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://gel.epfl.ch gel.epfl.ch
-zt2=`date +%s.%N`
-zduree=`jq -n $zt2-$zt1`
-echo -e $ztime" "$zduree
+
+echo -e "jahia.epfl.ch"
+    docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh https://jahia.epfl.ch jahia.epfl.ch
 
