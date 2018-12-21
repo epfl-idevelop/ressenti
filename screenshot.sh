648feebf47146b431c89156b4639d23fd8b4518f
Christian Zufferey <christian@zufferey.com>
2018-12-21 16:00:03 +0100

    Ca marche, mais je ne suis pas content car www.epfl.ch se charge 2x plus long sur mon docker que sur ma machine (8s au lieu de 4s), faut que je creuse encore

--- screenshot.sh
+++ screenshot.sh
@@ -1,5 +1,5 @@
 #Petit script pour faire une copie d'écran d'une page web avec firefox en mode headless
-#zf181221.1524
+#zf181221.1531
 
 #test si l'argument est vide
 if [ -z "$1" ]
@@ -19,6 +19,7 @@ firefox -headless -screenshot about-blank.png about:blank 2>/dev/null
 zt2=`date +%s.%N`
 zduree=`jq -n $zt2-$zt1`
 #echo -e "1er shot:" $zduree
+sleep 1
 
 zt1=`date +%s.%N`
 firefox -headless -screenshot about-blank.png about:blank 2>/dev/null
@@ -26,6 +27,7 @@ zt2=`date +%s.%N`
 zduree=`jq -n $zt2-$zt1`
 #echo -e "2e shot:" $zduree
 zoverlay=$zduree
+sleep 1
 
 zt1=`date +%s.%N`
 timeout 31 firefox -headless -screenshot $zpath/$2.t1.png $1 2>/dev/null
@@ -35,7 +37,6 @@ chmod 777 $zpath/$2.t1.png 2>/dev/null
 
 zduree=`jq -n $zt2-$zt1-$zoverlay`
 if [ `echo "$zduree<0"|bc` -eq 1 ]; then
-        echo -e "C'est <0"
 	zduree=0
 fi
 
--- start.sh
+++ start.sh
@@ -2,14 +2,15 @@
 #ATTENTION: faire tourner dans le host du container docker-ressenti!
 #ATTENTION: y'a encore des paramètre écrit en dur dans le code !
 
-#zf181221.1425
+#zf181221.1537
 
 while true ; do
     echo -e "
-Et voilà, on est parti pour un tour de carousel
+Et voilà, c'est parti pour un tour de carousel
 "
     docker container rm -f -v docker-ressenti
     docker run -d -i -v `pwd`/:/root/work --name="docker-ressenti" docker-firefox-zf
+    sleep 3
     docker exec -it docker-ressenti /bin/bash /root/work/ressenti-1.sh
 #  sleep 10
 done
