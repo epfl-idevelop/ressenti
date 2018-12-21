4ab2eebb368e891a21dea4d9329b7d893cece849
Christian Zufferey <christian@zufferey.com>
2018-12-21 16:23:22 +0100

    Voilà j'ai trouvé le bug, par rapport à hier, je tuais le container à chaque tour de carousel, et ce n'est vraiment pas une bonne idée :-(

--- screenshot.sh
+++ screenshot.sh
@@ -1,5 +1,5 @@
 #Petit script pour faire une copie d'écran d'une page web avec firefox en mode headless
-#zf181221.1531
+#zf181221.1614
 
 #test si l'argument est vide
 if [ -z "$1" ]
@@ -14,25 +14,31 @@ rm -rf /root/.cache/
 zpath="/root/work/images"
 mv $zpath/$2.t1.png $zpath/$2.t0.png 2>/dev/null
 
+sync
+sync
+sleep 1
 zt1=`date +%s.%N`
 firefox -headless -screenshot about-blank.png about:blank 2>/dev/null
 zt2=`date +%s.%N`
 zduree=`jq -n $zt2-$zt1`
 #echo -e "1er shot:" $zduree
-sleep 1
 
+sync
+sync
+sleep 1
 zt1=`date +%s.%N`
 firefox -headless -screenshot about-blank.png about:blank 2>/dev/null
 zt2=`date +%s.%N`
 zduree=`jq -n $zt2-$zt1`
 #echo -e "2e shot:" $zduree
 zoverlay=$zduree
-sleep 1
 
+sync
+sync
+sleep 1
 zt1=`date +%s.%N`
 timeout 31 firefox -headless -screenshot $zpath/$2.t1.png $1 2>/dev/null
 zt2=`date +%s.%N`
-
 chmod 777 $zpath/$2.t1.png 2>/dev/null
 
 zduree=`jq -n $zt2-$zt1-$zoverlay`
--- start.sh
+++ start.sh
@@ -2,15 +2,16 @@
 #ATTENTION: faire tourner dans le host du container docker-ressenti!
 #ATTENTION: y'a encore des paramètre écrit en dur dans le code !
 
-#zf181221.1537
+#zf181221.1610
+
+docker container rm -f -v docker-ressenti
+docker run -d -i -v `pwd`/:/root/work --name="docker-ressenti" docker-firefox-zf
+sleep 3
 
 while true ; do
     echo -e "
 Et voilà, c'est parti pour un tour de carousel
 "
-    docker container rm -f -v docker-ressenti
-    docker run -d -i -v `pwd`/:/root/work --name="docker-ressenti" docker-firefox-zf
-    sleep 3
     docker exec -it docker-ressenti /bin/bash /root/work/ressenti-1.sh
 #  sleep 10
 done
