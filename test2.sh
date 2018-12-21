1ad38f2b994617a03e039730b90c91617e93d136
Christian Zufferey <christian@zufferey.com>
2018-12-21 09:22:30 +0100

    Cela a tourné toute la nuit, mais il y a encore des mesures de l'âge du capitaine :-( * il y a des mesures négatives pendant plusieurs minutes, ce n'est pas normal ! * il y a aussi des blocages, il faut ajouter un timeout * on peut consulter les graphs ici: https://snapshot.raintank.io/dashboard/snapshot/JTmJKZuYGielvA3n4uWV5G45VDNq2Odl

--- test2.sh
+++ test2.sh
@@ -1,18 +1,18 @@
 #Petit script pour tester si cela peut tourner en boucle juste pour un site web
-#zf181219.1724
+#zf181220.1423
 
 time_page() {
     label=$1
     url=$2
+    img=$3
 
-    ztime=`date +%Y%m%d.%H%M%S-%N`
-    zt1=`date +%s.%N`
-    docker exec -it docker-firefox /bin/bash /root/work/test1.sh
-    zt2=`date +%s.%N`
-    zduree=`jq -n $zt2-$zt1`
+    ztime=`date +%Y%m%d.%H%M%S`
+    zduree=`docker exec -it docker-firefox /bin/bash /root/work/screenshot.sh $url $img`
     echo -e $ztime" "$zduree
 
-    t=$zduree
+    t=${zduree::-1}
+
+#    t=$zduree
     #  t=$(echo "2000 + $RANDOM % 3000" | bc)
     c=$(echo "$RANDOM % 2" | bc)
     cat <<__EOF | curl --data-binary @- http://localhost:9091/metrics/job/zuzuresenti/instance/$label
@@ -24,9 +24,32 @@ __EOF
 }
 
 while true ; do
-  time_page "www.epfl.ch"  "https://www.epfl.ch/"
-#  time_page "enac"  "https://www.epfl.ch/schools/enac/"
-  sleep 10
+
+echo -e "z.zufferey.com"
+time_page "z.zufferey.com" "http://z.zufferey.com" "z.zufferey.com"
+
+echo -e "www.epfl.ch"
+time_page "www.epfl.ch" "https://www.epfl.ch" "www.epfl.ch"
+
+echo -e "www.epfl.ch/wp-admin"
+time_page "www.epfl.ch-wp-admin" "https://www.epfl.ch/wp-admin" "www.epfl.ch-wp-admin"
+
+echo -e "actu.epfl.ch"
+time_page "actu.epfl.ch" "https://actu.epfl.ch" "actu.epfl.ch"
+
+echo -e "memento.epfl.ch"
+time_page "memento.epfl.ch" "https://memento.epfl.ch" "memento.epfl.ch"
+
+echo -e "people.epfl.ch"
+time_page "people.epfl.ch" "https://people.epfl.ch/christian.zufferey" "people.epfl.ch"
+
+echo -e "gel.epfl.ch"
+time_page "gel.epfl.ch" "https://gel.epfl.ch" "gel.epfl.ch"
+
+echo -e "jahia.epfl.ch"
+time_page "jahia.epfl.ch" "https://jahia.epfl.ch" "jahia.epfl.ch"
+
+#  sleep 10
 done
 
 
