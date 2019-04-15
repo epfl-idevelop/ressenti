7fc24db65d8ae1d7a91d6a62732bc9dbba3d9de5
Christian Zufferey <christian@zufferey.com>
2019-04-15 16:04:14 +0200

    wip

--- tst_flask/async_ressenti.py
+++ tst_flask/async_ressenti.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 #Deuxième version toute simple du web service ressenti en mode asynchrone
-zapli="\n\nsync_ressenti.py   zf190415.1144 \n"
+zapli="\n\nsync_ressenti.py   zf190415.1603 \n"
 zusage="Usage: http://siipc6.epfl.ch:5050/url/http/z.zufferey.com\n\n"
 
 #source: http://sdz.tdct.org/sdz/creez-vos-applications-web-avec-flask.html#Premierspas
@@ -14,22 +14,39 @@ import time
 import os
 import subprocess
 
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-print (zapli)
-print(zusage)
 
-@app.route('/url/<proto>/<url>')
-def index(proto,url):
+
+
+
+@app.route('/toto')
+def toto():
+    zurl = request.args.get('url')
+    print("url: " + zurl)
+
+    return "c'est ok !"
+
+
+
+@app.route('/')
+def index():
+    zurl = request.args.get('url')
+    print("url: " + zurl)
+    zimg = zurl.replace('http://', '')  ;  zimg = zimg.replace('https://', '')  ;  zimg = zimg.replace('/', '_')
+    print("img: " + zimg)
+
     start = time.time()
 #    cmd = "timeout 31 firefox -headless -screenshot toto.png " + proto + "://" + url + " 2>/dev/null"
-    cmd = "docker exec -it docker-ressenti /bin/bash /root/work/screenshot.sh  " + proto + "://" + url + " " + url
+    cmd = "docker exec -it docker-ressenti /bin/bash /root/work/screenshot.sh  " + zurl + " " + zimg
     os.system(cmd)
     end = time.time()
     result = str(end-start)
-    return zapli + "ça a tourné en " + result + " secondes" 
+    return zapli + "<br><br>ça a tourné en " + result + " secondes" 
 
 
 if __name__ == '__main__':
+    print (zapli)
+    print(zusage)
     app.run(host='0.0.0.0',port=5000, debug=True)
