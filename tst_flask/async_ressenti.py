44e56b2b6a5920993a398cea971ee1046655e111
Christian Zufferey <christian@zufferey.com>
2019-04-16 10:13:12 +0200

    wip

--- tst_flask/async_ressenti.py
+++ tst_flask/async_ressenti.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 #Deuxième version toute simple du web service ressenti en mode asynchrone
-zapli="\n\n async_ressenti.py   zf190416.0840 \n"
+zapli="\n\n async_ressenti.py   zf190416.0929 \n"
 zusage="Usage: http://siipc6.epfl.ch:5050/url/http/z.zufferey.com\n\n"
 
 #source: http://sdz.tdct.org/sdz/creez-vos-applications-web-avec-flask.html#Premierspas
@@ -11,6 +11,7 @@ zusage="Usage: http://siipc6.epfl.ch:5050/url/http/z.zufferey.com\n\n"
 #source: https://openclassrooms.com/fr/courses/235344-apprenez-a-programmer-en-python/2235545-faites-de-la-programmation-parallele-avec-threading
 
 
+import datetime
 import time
 import os
 import subprocess
@@ -20,19 +21,21 @@ from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 zstack = []
+ztime_table = dict()
 
 
-class zget_time(threading.Thread):
+class zbench_time(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
         while str(threading.enumerate()[0]).find("started") > 0 :
             while len(zstack) > 0 :
-                print("zstack: " + str(zstack))
+                print("zbench_time zstack: " + str(zstack))
                 zurl = zstack[0]
                 print("je mesure: " + zurl)
                 time.sleep(3)               #je vais mesurer l'url :-)
+                ztime_table[zurl][0] = datetime.datetime.now()
                 print("url retiree: " + zstack.pop(0))        #je retire l'url du fifo
             time.sleep(2)
             print("je boucle :-)")
@@ -40,6 +43,12 @@ class zget_time(threading.Thread):
             print("thread.enumerate: " + str(str(threading.enumerate()[0]).find("started")))
 
 
+
+def zget_time(zget_url):
+    if ztime_table.get(zget_url, True):
+        ztime_table[zget_url]={}
+        ztime_table[zget_url][0] = "0"
+    return ztime_table[zget_url][0]
 
 
 
@@ -49,9 +58,10 @@ def toto():
     zurl = request.args.get('url')
     print("url: " + zurl)
     zstack.append(zurl)
-    print(zstack)
+    print("route toto zstack: " + str(zstack))
+    print("route toto ztime_table: " + str(ztime_table))
 
-    return "c'est ok !"
+    return zapli + "<br><br>c'est ok !  " + zget_time(zurl)
 
 
 
@@ -77,11 +87,15 @@ def index():
     return zapli + "<br><br>ça a tourné en " + result + " secondes" 
 
 
+
+
+
+
 if __name__ == '__main__':
     print (zapli)
     print(zusage)
 
-    thread_1 = zget_time()
+    thread_1 = zbench_time()
     thread_1.start()
     app.run(host='0.0.0.0',port=5000, debug=True, use_reloader=False)
 
