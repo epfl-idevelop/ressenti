0590b3d7bd0b2add02c29fd503b612a199ce86e5
Christian Zufferey <christian@zufferey.com>
2019-04-29 15:17:00 +0200

    Ajouté le srcipt stop.sh :-)

--- stop.sh
+++ stop.sh
@@ -2,7 +2,8 @@
 #Petit script pour arrêter le binz
 #ATTENTION: faire tourner dans le host du container docker-ressenti!
 
-#zf190424.1625
+#zf190424.1746
 
 pkill -9 -f '/usr/bin/python tst_flask/async_ressenti.py'
+pkill -9 -f '/usr/bin/python ./tst_flask/async_ressenti.py'
 
